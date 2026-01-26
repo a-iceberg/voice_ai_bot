30709ba70d2491addb768c511ab2fea23cddbf44
Stepan Martynovich <martynovich.s@gmail.com>
2026-01-26 22:30:19 +0000

    add utm_source, update logging and config

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 const { initializeAriClient } = require('./asterisk');
-const { config, logger } = require('./config');
+const { logger } = require('./config');
+
+process.on('uncaughtException', (err) => {
+  logger.error('UNCAUGHT EXCEPTION', err);
+});
+
+process.on('unhandledRejection', (reason) => {
+  logger.error('UNHANDLED REJECTION', reason);
+});
+
+process.on('SIGTERM', () => {
+  logger.warn('SIGTERM received, shutting down...');
+  process.exit(0);
+});
+
+process.on('SIGINT', () => {
+  logger.warn('SIGINT received, shutting down...');
+  process.exit(0);
+});
 
 async function startApplication() {
   try {
     logger.info('Starting application');
     await initializeAriClient();
     logger.info('Application started successfully');
+
+    await new Promise(() => {});
   } catch (e) {
-    logger.error(`Startup error: ${e.message}`);
+    logger.error('Startup error', e);
     process.exit(1);
   }
 }
